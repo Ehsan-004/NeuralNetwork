47052964d8156a2d531dd526e1eeb5f5fcfe144d
Ehsan-004 <ehsankholusi@gmail.com>
2025-06-08 12:35:01 +0330

    snc | model works but not in the right way

--- single_nueron_classification.py
+++ single_nueron_classification.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from core.activations import sigmoid
+from core.loss import msq
 
 
 def random_data_generator(plot = False):
 
-    n_samples = 100
+    n_samples = 10000
 
     x_min, x_max = -10, 10
     y_min, y_max = -10, 30
@@ -21,7 +22,7 @@ def random_data_generator(plot = False):
 
     data = np.column_stack((X, Y, classes))
 
-    df = pd.DataFrame(data, columns=['x', 'y', 'class'])
+    df = pd.DataFrame(data, columns=['x1', 'x2', 'class'])
 
     if plot:
         plt.figure(figsize=(8, 8))
@@ -29,8 +30,8 @@ def random_data_generator(plot = False):
         x_vals = np.linspace(x_min, x_max, 100)
         plt.plot(x_vals, decision_boundary(x_vals), 'k--', label='y = 2x + 1')
         plt.title("Generated Data (First 2000 points)")
-        plt.xlabel("x")
-        plt.ylabel("y")
+        plt.xlabel("x1")
+        plt.ylabel("x2")
         plt.legend()
         plt.grid(True)
         plt.show()
@@ -49,6 +50,12 @@ def initialize_weights():
 
 
 def forward(weight1, weight2,  x1, x2, bias, activation):
+    # a = weight1*x1
+    # b = weight2*x2
+    # print(f"a = {a}")
+    # print(f"b = {b}")
+    # print(f"a+b = {a+b}")
+    # print(activation(19))
     return activation(weight1*x1 + weight2*x2 + bias)
 
 
@@ -57,7 +64,7 @@ def dl_dw_linear(xs: list, weights, class_predicted, true_class, loss_function, 
     """
     derivative of loss with respect to weight number weight number
     """
-    ws = []
+    dl_ws = []
     for w in range(len(weights)):
         for j in range(len(xs[0])):
             res = 0
@@ -65,31 +72,68 @@ def dl_dw_linear(xs: list, weights, class_predicted, true_class, loss_function, 
             sig = loss_function(weights[0] * xs[0][j] + weights[1] * xs[1][j] + bias)
             x = xs[w][j]
             res += error * (sig * (1-sig)) * x
-        ws.append(-2 * res)
-    return ws
+        dl_ws.append(-2 * res)
+    return dl_ws
 
 
-def linear_classification(dataset, epochs=10, learning_rate = 0.0001):
+def dl_db_linear(xs: list, weights, class_predicted, true_class, loss_function, bias):
+    dl_bs = []
+    for i in range(2):
+        error = true_class[i] - class_predicted[i]
+        z = weights[0] * xs[0][i] + weights[1] * xs[1][i] + bias
+        sig = loss_function(z)
+        res = ((sig * (1-sig)) * (error)) 
+        dl_bs.append(-2 * res)
+    return sum(dl_bs)
+
+
+def linear_classification(dataset, epochs=1000, learning_rate = 0.001):
     w1, w2, b = initialize_weights()
     
     print(f"Initial w1 = {w1:.4f}")
-    print(f"Initial w2 = {w2:.4f}")
+    print(f"Initial w2 = {w2:.4f}") 
     print(f"Initial b  = {b:.4f}")
             
-    x1_batch = dataset["x"]
-    x2_batch = dataset["y"]
+    x1_batch = dataset["x1"]
+    x2_batch = dataset["x2"]
+    classes = dataset["class"]
+    
+    print(len(x1_batch))
+    print(len(x2_batch))
     
     class_batch = list(dataset["class"])
-    # print(x1_batch[:5])
-    # print(x2_batch[:5])
-
+   
+    print("starting training")
+   
     for epoch in range(epochs):
-        output = forward(w1, w2, x1_batch, x2_batch, b, sigmoid)
+        output = forward(w1, w2, np.array(x1_batch), np.array(x2_batch), b, sigmoid)
+        # print(output[:5])
         dl_dws = dl_dw_linear([x1_batch, x2_batch], [w1, w2], output, class_batch, sigmoid, b)
-        print(f"derivatives in epoch {epoch}")
-        print(dl_dws[:100])
+        dl_dbs = dl_db_linear([x1_batch, x2_batch], [w1, w2], output, class_batch, sigmoid, b)
+        # print(f"derivatives in epoch {epoch}")
+        # print(f"dl_dw1 = {dl_dws[0]}")
+        # print(f"dl_dw2 = {dl_dws[1]}")
+        # print(f"dl_db = {dl_dbs}")
+        w1 -= learning_rate * dl_dws[0]
+        w2 -= learning_rate * dl_dws[1]
+        b -= learning_rate * dl_dbs
+        # print()
+        # print(f"new w1 = {w1}")
+        # print(f"new w2 = {w2}")
+        # print(f"new b = {b}")
+        
+        loss = msq(classes, output)
+        
+        if (epoch + 1) % 10 == 0 or epoch == 0: 
+            print(f"Epoch {epoch + 1}/{epochs}, Loss: {loss:.8f}, w1: {w1:.6f}, w2: {w2:.6f}, Current b: {b:.6f}")
+        
+    
+    
         # print(output[:2])
-
+    print("training finished")
+    print(f"final w1 = {w1}")
+    print(f"final w2 = {w2}")
+    print(f"final b = {b}")
 
 
 if __name__ == "__main__":
@@ -101,6 +145,8 @@ if __name__ == "__main__":
     # f = [1,1,1,1]
     # print(dl_dw_linear(x, ws, f, y, sigmoid, bis))
         
+    # print(forward(2, 3, np.array([1,2,3]), np.array([4,5,6]), 5, sigmoid))
+        
     dataset = random_data_generator(plot=False)
-    print(dataset.head())
-    # linear_classification(dataset)
+    dataset["class"] = dataset["class"].astype(int)
+    linear_classification(dataset)
