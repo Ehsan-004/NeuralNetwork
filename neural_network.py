73b446a4237a9c1ff88cec22dce9da93427a5cb4
Ehsan-004 <ehsankholusi@gmail.com>
2025-06-18 23:02:34 +0330

    nn | layer definition and forward pass works

--- neural_network.py
+++ neural_network.py
@@ -4,29 +4,28 @@ from core.activations import ReLU, softmax
 
 class Neuron:
     def __init__(self, input_num):
-        self.input_weigts = initialize_weights(input_num)
-        self.bias = initialize_weights(1)[0]
+        self.input_weigts = initialize_weights(input_num, w=0.01)
+        self.bias = initialize_weights(1, w=0.01)[0]
         
     
     def forward(self, x):
         y = sum([(x_value*weight) for x_value, weight in zip(x, self.input_weigts)]) + self.bias
         return y
+    
+    def get_weights(self):
+        return self.input_weigts
+    
+    def get_bias(self):
+        return self.bias
 
-
-class InputNeuron(Neuron):
-    def __init__(self, output_num):
-        super().__init__(output_num)
 
 
 class Layer:
-    def __init__(self, previous_layer_neurons_num,  neuron_num):
+    def __init__(self, input_neurons,  neuron_num):
             
-        self.previous_neuron_num = previous_layer_neurons_num
-        self.neurons = [Neuron(previous_layer_neurons_num) for i in range(neuron_num)]
+        self.previous_neuron_num = input_neurons
+        self.neurons = [Neuron(input_neurons) for i in range(neuron_num)]
         
-        # print(f"this layer has {len(self.neurons)} neurons!")
-        # for j in range(len(self.neurons)):
-        #     print(f"neuron number {j+1} | {self.neurons[j].input_weigts}")
         
     def forward(self, x):
         if len(x) != self.previous_neuron_num:
@@ -36,33 +35,43 @@ class Layer:
         return out
     
     
+    def get_weights(self):
+        # each line of this list shows input weight to a neuron
+        out = [nr.get_weights() for nr in self.neurons]
+        return out
     
     
-# class InputLayer:
-    # def __init__(self,  neuron_num):
-    #     self.neuron_num = neuron_num
-    #     # self.neurons = [Neuron(previous_layer_neurons_num) for i in range(neuron_num)]
-    #     self.activation = activation_function
-        
-    #     # print(f"this layer has {len(self.neurons)} neurons!")
-    #     # for j in range(len(self.neurons)):
-    #     #     print(f"neuron number {j+1} | {self.neurons[j].input_weigts}")
-        
-    # def forward(self, x):
-    #     if len(x) != self.previous_neuron_num:
-    #         raise ValueError(f"x must have len {self.previous_neuron_num} but got len {len(x)}")
-        
-    #     out = []
-    #     for nr in self.neurons:
-    #         out.append(self.activation(nr.forward(x)))
-        
-    #     return out
-
-
     
+class NeuralNetwork:
+    def __init__(self, layers_activation_pair: list[Layer]):
+        self.layers = layers_activation_pair  # leyer, activation function
+        self.depth = len(self.layers)
+         
+        
+    def forward(self, x):
+        if len(x) != self.layers[0][0].previous_neuron_num:
+            raise ValueError(f"x must have len {self.layers[0].previous_neuron_num} but got len {len(x)}")
+        
+        self.activations = []
+        
+        for layer, activation in self.layers:
+            x = activation(layer.forward(x))
+            self.activations.append(x)
+        return x
 
+
+    def calculate_deltas(self):
+        deltas = []
+        for i in range(self.depth, 0, -1):
+            deltas.append()
+        return
+
+
+    def get_activations(self):
+        return self.activations
 
 if __name__ == "__main__":
+    from pprint import pprint
     # ne = Neuron(1)
     # print(f"output weights: {ne.input_weigts}")
     # print(f"bias: {ne.bias}")
@@ -70,22 +79,32 @@ if __name__ == "__main__":
     # y = ne.forward([1,1,1])
     # print(f"y: {y}")
     
-    le1 = Layer(10, 10)
-    le2 = Layer(10, 5)
-    le3 = Layer(5, 2)
+    # ==============================================================================================
+    
+    # le = Layer(2, 3)
+    # a = le.get_weights()
+    # pprint(a)
+    
+    # ==============================================================================================
     
     i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print(i)
-    x = le1.forward(i)
-    xa = ReLU(x)
-    print(f"x before active: {x}")
-    print(f"x after active: {xa}")
-    y = le2.forward(x)
-    ya = ReLU(y)
-    print(f"y before active: {y}")
-    print(f"y after active: {ya}")
-    z = le3.forward(y)
-    za = softmax(z)
-    print(f"z before active: {z}")
-    print(f"z after active: {za}")
-    print(sum(za))
+    
+    layers = [[Layer(10, 10), ReLU], [Layer(10, 5), ReLU], [Layer(5, 2), softmax]]
+    nn = NeuralNetwork(layers)
+    print(nn.forward(i))
+    print(50*"-=")
+    
+    # print(i)
+    # x = layers[0][0].forward(i)
+    # xa = ReLU(x)
+    # print(f"x before active: {x}")
+    # print(f"x after active: {xa}")
+    # y = layers[1][0].forward(x)
+    # ya = ReLU(y)
+    # print(f"y before active: {y}")
+    # print(f"y after active: {ya}")
+    # z = layers[2][0].forward(y)
+    # za = softmax(z)
+    # print(f"z before active: {z}")
+    # print(f"z after active: {za}")
+    # print(sum(za))
